7d00893f4f6f728feef212a5cf5ffd6e0e3dc28c
Fabio313 <fabiozf3108@gmail.com>
2024-03-28 20:20:42 -0300

    Port config base classes

---
+++ src/main/java/br/edu/uniara/dpi2/fabio/model/Employee.java
@@ -0,0 +1,16 @@
+package br.edu.uniara.dpi2.fabio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String role;
+}
